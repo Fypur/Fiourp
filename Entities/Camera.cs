3efe5444f3b9a887f7da776704137af4361e006f
Fypur <84095553+Fypur@users.noreply.github.com>
2026-05-17 21:03:41 +0200

    Cleaned up camera

--- Entities/Camera.cs
+++ Entities/Camera.cs
@@ -64,7 +64,7 @@ namespace Fiourp
         }
 
 
-        public Camera(Vector2 position, float rotation, float zoomLevel)
+        public Camera(Vector2 position, float rotation, float zoomLevel) : base(position)
         {
             Engine.Cam = this;
 
@@ -79,47 +79,10 @@ namespace Fiourp
         public void Refresh()
             => needToRecalculateMatrix = true;
 
-        public void LightShake()
-            => Shake(0.2f, 1);
-
-        public void Shake(float time, float intensity)
-        {
-            Shaker shaker = GetComponent<Shaker>();
-            if (shaker == null || time > shaker.Time || intensity > shaker.Intensity)
-            {
-                RemoveComponent(shaker);
-                AddComponent(new Shaker(time, intensity, () => CenteredPos));
-            }
-        }
-
-        public void SetBoundaries(Rectangle bounds)
-        {
-            this.Bounds = bounds;
-            CenteredPos = CenteredPos;
-        }
-
-        public void SetBoundaries(Vector2 position, Vector2 size)
-        {
-            Bounds = new Rectangle(position.ToPoint(), size.ToPoint());
-            CenteredPos = CenteredPos;
-        }
-
         public Vector2 WorldToScreenPosition(Vector2 position)
             => Vector2.Transform(position, ViewMatrix);
 
         public Vector2 ScreenToWorldPosition(Vector2 position)
             => Vector2.Transform(position, InverseViewMatrix);
-
-        public Vector2 RenderTargetToScreenPosition(Vector2 position)
-            => position * ScreenSizeCoef;
-
-        public Vector2 ScreenToRenderTargetPosition(Vector2 position)
-            => position / ScreenSizeCoef;
-
-        public Vector2 ScreenToCamPosition(Vector2 position)
-            => position * (float)Engine.Cam.Width / Engine.RenderTarget.Width / ScreenSizeCoef;
-
-        public Vector2 RenderTargetToWorldPosition(Vector2 position)
-            => position + Engine.Cam.WorldToScreenPosition(position) * (Engine.Cam.ScreenSizeCoef - 1);
     }
 }
